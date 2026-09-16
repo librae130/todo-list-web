1b8d36a0dedc346f9b89ae68a007ef68a0284d28
librae <dohieuquang@gmail.com>
2026-09-16 13:57:54 +0700

    moved search todo to service and swapped unitofwork for concrete repos

--- backend/Services/UserService.cs
+++ backend/Services/UserService.cs
@@ -2,23 +2,24 @@ using AutoMapper;
 using backend.Data;
 using backend.Dtos;
 using backend.Entities;
+using backend.Repositories;
 
 namespace backend.Services;
 
 public class UserService
 {
-    private readonly IUnitOfWork _unitOfWork;
+    private readonly IGenericRepository<User> _userRepo;
     private readonly IMapper _mapper;
 
     public UserService(IUnitOfWork unitOfWork, IMapper mapper)
     {
-        _unitOfWork = unitOfWork;
+        _userRepo = unitOfWork.GetRepository<User>();
         _mapper = mapper;
     }
 
     public async Task<UserDto?> GetUserByIdAsync(Guid userId, CancellationToken ct)
     {
-        User? user = await _unitOfWork.GetRepository<User>().GetByIdAsync(userId);
+        User? user = await _userRepo.GetByIdAsync(userId, ct);
 
         if (user == null)
         {
